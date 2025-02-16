cea3739deeb1f5640aac928a0ba68c80948f047c
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2025-02-16 23:27:20 +0300

    Add open_vlc_player./Feature

---
+++ open_vlc_player.ps1
@@ -0,0 +1,9 @@
+param(
+	[string] $blob
+)
+
+$agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0" 
+$referrer = "https://aniboom.one/"
+$vlc = "C:\Program Files\VideoLAN\VLC\vlc.exe"
+
+& $vlc --http-referrer $referrer --http-user-agent $agent $blob
