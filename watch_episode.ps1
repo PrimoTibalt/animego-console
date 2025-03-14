755cdb89005b0abccfe7f4f6335ef8d223e6105b
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2025-03-14 03:29:11 +0300

    Rework watch_episode.ps1 / Feature
    Now it only retrieves m3u8/mpd link
    from json response from embed.
    Add temp folder to store some log
    data.

--- watch_episode.ps1
+++ watch_episode.ps1
@@ -1,49 +1,17 @@
 param(
-	[Parameter(Mandatory, Position = 1)]
-	[Int32]$number,
-	[Parameter(Position = 2)]
-	[Int32]$translation = 30,
 	[Parameter(Mandatory, Position = 0)]
-	[String]$link
+	[string]$link
 )
 
 Add-Type -AssemblyName 'System.Net'
-$link -match "-(?<number>\d\d\d\d)" > $null
 $headers = @{
-	'x-requested-with' = 'XMLHttpRequest'
-	'Referer'        = $link
-	'User-Agent'     = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0'
-	'Accept'         = 'application/json, text/javascript, */*; q=0.01'
+	'Referer'        = 'https://animego.one/'
+	'User-Agent'     = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0'
+	'Accept'         = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
 }
 
-$links = @( 'animego.me', 'animego.club', 'animego.org' )
-foreach ($postfixVariant in $links) {
-	$playerLink = "https://$postfixVariant/anime/" + $Matches.number + '/player?_allow=true'
-	try {
-		$response = Invoke-RestMethod -Uri $playerLink -Method 'Get' -Headers $headers -TimeoutSec 1
-		if ($response.status -eq 'success') {
-			Write-Output "Successfuly retrieved from $postfixVariant"
-			$html = $response.content
-			break;
-		}
-	}
-	catch {
-		Write-Output "$postfixVariant did not succeed"
-		continue;
-	}
-}
-
-if ($null -eq $html) {
-	Write-Output 'Content is empty'
-	return;
-}
-
-$html -match "//aniboom`.one/embed/(?<hash>[^?]*)?" > $null
-
-$animeUrl = 'https://aniboom.one/embed/' + $Matches.hash + `
-						"?episode=$number&translation=$translation"
-$headers.Referer = 'https://animego.me/'
-$html = Invoke-RestMethod -Uri $animeUrl -Method 'Get' -Headers $headers -TimeoutSec 1
+Write-Host $link
+$html = Invoke-RestMethod -Uri $link -Method 'Get' -Headers $headers -TimeoutSec 5
 
 $html = [System.Net.WebUtility]::HtmlDecode($html)
 $html -match "data-parameters=`"{(?<parameters>[^ ]*)}`"" > $null
@@ -53,10 +21,10 @@ switch -regex ($json.hls)
 {
 	'(?<link>https:[^ ]*m3u8)' {
 		$m3u8 = $Matches.link.Replace('\/', '/')
-		& .\open_vlc_player.ps1 -blob $m3u8
+		& ./helpers/open_vlc_player.ps1 -blob $m3u8
 	}
 	'(?<link>https:[^ ]*mpd)' {
 		$mpv = $Matches.link.Replace('\/', '/')
-		& .\open_vlc_player.ps1 -blob $mpv
+		& ./helpers/open_vlc_player.ps1 -blob $mpv
 	}
 }
