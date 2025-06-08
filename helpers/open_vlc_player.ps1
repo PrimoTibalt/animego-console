564b272c64e5f8df3760d9eff0526af51a85900b
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2025-06-08 23:12:35 +0300

    Add vlc player installation in case it is missing / Feature

--- helpers/open_vlc_player.ps1
+++ helpers/open_vlc_player.ps1
@@ -3,8 +3,14 @@ param(
 	[string] $blob
 )
 
-$agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0" 
-$referrer = "https://aniboom.one/"
-$vlc = "C:\Program Files\VideoLAN\VLC\vlc.exe"
+$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0' 
+$referrer = 'https://aniboom.one/'
+$vlc = 'C:\Program Files\VideoLAN\VLC\vlc.exe'
+
+if (-not (Test-Path $vlc)) {
+	Write-Host "You don't seem to have VLC player installed, let me change that..."
+	winget install --id VideoLAN.VLC --silent
+	. "$PSScriptRoot/clean_console.ps1" 10 # Not very reliable ain't it
+}
 
 & $vlc --http-referrer $referrer --http-user-agent $agent $blob --fullscreen
